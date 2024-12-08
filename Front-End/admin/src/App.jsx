94c377eb71008ccd94ef248f6dc383b5bc8577aa
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-09 01:42:36 +0700

    feat: create App component

--- Front-End/admin/src/App.jsx
+++ Front-End/admin/src/App.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import SideBar from './components/SideBar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import DoctorsList from './pages/Admin/DoctorsList';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
 
 const App = () => {
-  return (
-    <div>
-      
+  const { aToken } = useContext(AdminContext);
+
+  return aToken !== null ? (
+    <div className='bg-[#F8F9FD]'>
+      <ToastContainer />
+      <Navbar />
+      <div className='flex items-start'>
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appoinments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   )
 }
 
