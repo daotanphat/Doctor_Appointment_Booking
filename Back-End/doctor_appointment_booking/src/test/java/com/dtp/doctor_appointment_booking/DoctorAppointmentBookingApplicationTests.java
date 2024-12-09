a509c263e7265963762b25450195e64c7edd31f9
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-09 22:51:09 +0700

    Doctor Appointment Booking

---
+++ Back-End/doctor_appointment_booking/src/test/java/com/dtp/doctor_appointment_booking/DoctorAppointmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtp.doctor_appointment_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
