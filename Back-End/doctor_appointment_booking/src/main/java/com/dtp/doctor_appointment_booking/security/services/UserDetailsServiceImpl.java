46e31d7f2e40771aa0c330da0f73949549387319
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-11 23:41:21 +0700

    feat: sign up & login using JWT

---
+++ Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.dtp.doctor_appointment_booking.security.services;
+
+import com.dtp.doctor_appointment_booking.model.User;
+import com.dtp.doctor_appointment_booking.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
