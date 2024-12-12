cef8cc9bc5f86c1d3796e70c890434dc8cdbe3b7
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-12 17:32:42 +0700

    feat: create User & Role repository

---
+++ Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.dtp.doctor_appointment_booking.repository;
+
+import com.dtp.doctor_appointment_booking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+}
