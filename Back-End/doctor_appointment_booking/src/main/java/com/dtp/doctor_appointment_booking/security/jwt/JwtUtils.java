76a688a96c31e357f1ca04fdc0a1ab1fee6914e9
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-12 17:30:12 +0700

    fix: fix map value from file properties

--- Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/security/jwt/JwtUtils.java
+++ Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/security/jwt/JwtUtils.java
@@ -18,10 +18,10 @@ import java.util.Date;
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("$app.jwtSecret")
+    @Value("${app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("$app.jwtExpirationMs")
+    @Value("${app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
