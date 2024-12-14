6cff0f0c1727b04d593db5a74575a9c214314611
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-14 23:13:10 +0700

    refactor: config to connect with backend

--- Front-End/doctor_appointment_booking/src/main.jsx
+++ Front-End/doctor_appointment_booking/src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
+import { Provider } from 'react-redux'
+import { store } from '../state/Store'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </AppContextProvider>
   </BrowserRouter>,
 )
