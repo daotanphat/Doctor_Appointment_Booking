72ddc54c94fba1cc8be6ee7cb35f21fe1cfe7dba
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-21 23:18:58 +0700

    feat: create response DTO include totalPages attribute

---
+++ Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/dto/response/PageResponse.java
@@ -0,0 +1,15 @@
+package com.dtp.doctor_appointment_booking.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+    private List<T> objects;
+    private int totalPages;
+}
