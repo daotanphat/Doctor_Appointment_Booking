240322f701e3bef21d9c0f30e57e2094f2efed47
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-23 23:51:30 +0700

    feat: create enpoint service to define all enpoint

---
+++ Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/service/EndPointService.java
@@ -0,0 +1,26 @@
+package com.dtp.doctor_appointment_booking.service;
+
+public class EndPointService {
+    public static final String[] publicPostEndPoint = {
+            "/api/auth/signup",
+            "/api/auth/signin",
+            "/api/auth/refresh-token",
+            "/api/auth/logout"
+    };
+
+    public static final String[] adminGetEndPoint = {
+            "/api/admin/specialities"
+    };
+
+    public static final String[] adminPostEndPoint = {
+            "/api/admin/add-doctor",
+            "/api/admin/add-speciality"
+    };
+
+    public static final String[] userGetEndPoint = {
+            "/api/user/specialities",
+            "/api/user/doctor-list",
+            "/api/user/doctor/{id}",
+            "/api/user/doctor/speciality/{speciality}"
+    };
+}
