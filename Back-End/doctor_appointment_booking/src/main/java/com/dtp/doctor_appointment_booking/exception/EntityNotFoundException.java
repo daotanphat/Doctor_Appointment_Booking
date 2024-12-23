f0d4a5536ebfd41baaecf20bd3e226b3ba9a1012
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-23 23:53:24 +0700

    refactor: custom message response

--- Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/exception/EntityNotFoundException.java
+++ Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/exception/EntityNotFoundException.java
@@ -1,7 +1,7 @@
 package com.dtp.doctor_appointment_booking.exception;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(Class<?> entity, int id) {
-        super("The " + entity.getSimpleName().toLowerCase() + " with id " + id + " was not found.");
+    public EntityNotFoundException(Class<?> entity) {
+        super("The " + entity.getSimpleName().toLowerCase() + " was not found.");
     }
 }
