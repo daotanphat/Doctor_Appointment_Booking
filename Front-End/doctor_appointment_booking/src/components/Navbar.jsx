1df6cb3b805f84afa0d97b456175c3aeda02963c
Dao Tan Phat <daotanphat2143@gmail.com>
2024-12-26 01:34:44 +0700

    refactor: fetch user info

--- Front-End/doctor_appointment_booking/src/components/Navbar.jsx
+++ Front-End/doctor_appointment_booking/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { logout } from '../../state/Authentication/Actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
+import { getUserInfo } from '../../state/user/Actions';
+import avatarDefault from '../assets/avatar_default.jpg';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -11,15 +13,19 @@ const Navbar = () => {
 
   const successMessage = useSelector((state) => state.auth.message);
   const email = useSelector((state) => state.auth.user);
+  const user = useSelector((state) => state.user.user);
 
   const [showMenu, setShowMenu] = useState(false);
-  const [token, setToken] = useState(true);
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, []);
 
   const logoutHandler = async (event) => {
     try {
       await dispatch(logout(email));
       toast.success(successMessage || 'Logout success!');
-      setToken(false);
       navigate('/login');
     } catch (error) {
       // Assuming the error is set in the Redux state
@@ -52,7 +58,7 @@ const Navbar = () => {
         {
           token ?
             <div className='flex items-center gap-2 cursor-pointer group relative'>
-              <img className='w-8 rounded-full' src={assets.profile_pic} alt='' />
+              <img className='w-8 rounded-full' src={user?.imageUrl || avatarDefault} alt='' />
               <img className='w-2.5' src={assets.dropdown_icon} alt='' />
               <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                 <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
