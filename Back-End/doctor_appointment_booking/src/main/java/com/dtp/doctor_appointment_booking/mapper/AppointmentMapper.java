625199eb3462e3c20fd24df4a9e8225a393e6aa4
Dao Tan Phat <daotanphat2143@gmail.com>
2025-01-01 01:04:24 +0700

    fix: handle error to map from entity to dto

--- Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/mapper/AppointmentMapper.java
+++ Back-End/doctor_appointment_booking/src/main/java/com/dtp/doctor_appointment_booking/mapper/AppointmentMapper.java
@@ -5,34 +5,50 @@ import com.dtp.doctor_appointment_booking.dto.appointment.BookAppointmentRequest
 import com.dtp.doctor_appointment_booking.model.Appointment;
 import com.dtp.doctor_appointment_booking.model.AppointmentStatus;
 import com.dtp.doctor_appointment_booking.model.Status;
+import com.dtp.doctor_appointment_booking.repository.AppointmentStatusRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-@Mapper
-public interface AppointmentMapper {
-    AppointmentMapper INSTANCE = Mappers.getMapper(AppointmentMapper.class);
+@Mapper(componentModel = "spring")
+public abstract class AppointmentMapper {
+    @Autowired
+    AppointmentStatusRepository appointmentStatusRepository;
 
     @Mapping(target = "doctor", ignore = true)
     @Mapping(target = "patient", ignore = true)
     @Mapping(target = "timeSlotFrom.id", source = "timeSlotFrom")
     @Mapping(target = "timeSlotTo.id", source = "timeSlotTo")
-    Appointment bookRequestToEntity(BookAppointmentRequest request);
+    public abstract Appointment bookRequestToEntity(BookAppointmentRequest request);
 
-    @Mapping(target = "doctor", source = "doctor.fullName")
-    @Mapping(target = "patient", source = "patient.fullName")
-    @Mapping(target = "timeSlotFrom", source = "timeSlotFrom.time")
-    @Mapping(target = "timeSlotTo", source = "timeSlotTo.time")
-    @Mapping(target = "status", expression = "java(mapStatus(appointment))")
-    AppointmentResponse entityToResponse(Appointment appointment);
+    public AppointmentResponse entityToResponse(Appointment appointment) {
+        AppointmentStatus appointmentStatus = appointmentStatusRepository
+                .findByAppointmentIdOrderByCreatedAtDesc(appointment.getAppointment_id());
+        return new AppointmentResponse(
+                appointment.getAppointment_id(),
+                appointment.getDoctor().getFullName(),
+                appointment.getPatient().getFullName(),
+                appointment.getFee(),
+                appointment.getDateSlot(),
+                appointment.getTimeSlotFrom().getTime(),
+                appointment.getTimeSlotTo().getTime(),
+                appointment.getAddress(),
+                appointmentStatus.getStatus().getStatus(),
+                appointment.getPaymentStatus(),
+                appointment.getCreatedAt()
+        );
+    }
 
-    List<AppointmentResponse> entitiesToResponses(List<Appointment> appointments);
+    public abstract List<AppointmentResponse> entitiesToResponses(List<Appointment> appointments);
 
-    default String mapStatus(Appointment appointment) {
-        return appointment.getStatuses().stream()
+    public String mapStatus(Appointment appointment) {
+        List<AppointmentStatus> statuses = new ArrayList<>(appointment.getStatuses());
+        return statuses.stream()
                 .max(Comparator.comparing(AppointmentStatus::getCreatedAt))
                 .map(AppointmentStatus::getStatus)
                 .map(Status::getStatus)
